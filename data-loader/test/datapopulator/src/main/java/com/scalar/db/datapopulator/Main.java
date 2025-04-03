cea51e126816578a2166fe8ae160a4c20873bc82
Jishnu J <jishnu.j@innovaturelabs.com>
2025-04-03 13:38:10 +0530

    Data puplator updated

--- data-loader/test/datapopulator/src/main/java/com/scalar/db/datapopulator/Main.java
+++ data-loader/test/datapopulator/src/main/java/com/scalar/db/datapopulator/Main.java
@@ -3,7 +3,8 @@ package com.scalar.db.datapopulator;
 import com.scalar.db.api.DistributedTransaction;
 import com.scalar.db.api.DistributedTransactionManager;
 import com.scalar.db.api.Put;
-import com.scalar.db.api.PutBuilder;
+import com.scalar.db.datapopulator.utils.RandomStringGenerator;
+import com.scalar.db.exception.transaction.TransactionException;
 import com.scalar.db.io.BlobColumn;
 import com.scalar.db.io.BooleanColumn;
 import com.scalar.db.io.DateColumn;
@@ -27,69 +28,88 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         Path scalarDbPropertiesFilePath = Paths.get(args[0]);
         int recordCount = Integer.parseInt(args[1]);
-        TransactionFactory transactionFactory =TransactionFactory.create(scalarDbPropertiesFilePath);
+
+        TransactionFactory transactionFactory = TransactionFactory.create(scalarDbPropertiesFilePath);
         DistributedTransactionManager transactionManager = transactionFactory.getTransactionManager();
+
         int coreCount = Runtime.getRuntime().availableProcessors();
-        int batchSize = 10; // Number of Put operations per transaction
+        int batchSize = 10;
         ExecutorService executorService = Executors.newFixedThreadPool(coreCount);
+        CompletionService<Void> completionService = new ExecutorCompletionService<>(executorService);
         AtomicInteger insertedRecords = new AtomicInteger(0);
+        int totalTasks = (int) Math.ceil((double) recordCount / batchSize);  // Ensure correct task tracking
+
         for (int i = 1; i <= recordCount; i += batchSize) {
             int finalI = i;
-            executorService.submit(
-                    () -> {
+            completionService.submit(() -> {
+                DistributedTransaction tx = null;
+                try {
+                    tx = transactionManager.start();
+                    List<Put> puts = new ArrayList<>(batchSize);
+
+                    for (int j = finalI; j < finalI + batchSize && j <= recordCount; j++) {
+                        Key partitionKey = Key.ofBigInt("col1", j);
+                        Key clusteringKey = Key.newBuilder()
+                                .add(IntColumn.of("col2", j))
+                                .add(BooleanColumn.of("col3", true))
+                                .build();
+
+                        Put put = Put.newBuilder()
+                                .namespace("test")
+                                .table("all_columns")
+                                .partitionKey(partitionKey)
+                                .clusteringKey(clusteringKey)
+                                .value(FloatColumn.of("col4", Float.MAX_VALUE))
+                                .value(DoubleColumn.of("col5", Double.MAX_VALUE))
+                                .value(TextColumn.of("col6", RandomStringGenerator.generateRandomString(100)))
+                                .value(BlobColumn.of("col7", RandomStringGenerator.generateRandomString(100).getBytes(StandardCharsets.UTF_8)))
+                                .value(DateColumn.of("col8", LocalDate.of(2000, 1, 1)))
+                                .value(TimeColumn.of("col9", LocalTime.of(1, 1, 1)))
+                                .value(TimestampColumn.of("col10", LocalDateTime.of(2000, 1, 1, 1, 1)))
+                                .value(TimestampTZColumn.of("col11", Instant.ofEpochMilli(1740041740)))
+                                .build();
+
+                        puts.add(put);
+                    }
+
+                    tx.put(puts);
+                    tx.commit();
+
+                    int currentInsertedRecords = insertedRecords.addAndGet(batchSize);
+                    if (currentInsertedRecords % 10000 == 0) {
+                        System.out.println("Total records inserted: " + currentInsertedRecords);
+                    }
+                } catch (TransactionException e) {
+                    if (tx != null) {
                         try {
-                            DistributedTransaction tx = transactionManager.start();
-                            List<Put> puts = new ArrayList<>();
-                            for (int j = finalI; j < finalI + batchSize && j <= recordCount; j++) {
-                                Key partitionKey = Key.ofBigInt("col1", j);
-                                Key clusteringKey =
-                                        Key.newBuilder()
-                                                .add(IntColumn.of("col2", j))
-                                                .add(BooleanColumn.of("col3", true))
-                                                .build();
-                                PutBuilder.Buildable buildable =
-                                        Put.newBuilder()
-                                                .namespace("test")
-                                                .table("all_columns")
-                                                .partitionKey(partitionKey);
-                                buildable.clusteringKey(clusteringKey);
-                                buildable.value(FloatColumn.of("col4", Float.MIN_VALUE));
-                                buildable.value(DoubleColumn.of("col5", Double.MIN_VALUE));
-                                buildable.value(TextColumn.of("col6", "VALUEhhshgsdjgjasdgjhgasdbbnbfmsabmfjdlkjlksjakldhajgsdm!!s"));
-                                buildable.value(
-                                        BlobColumn.of("col7", "blob test value".getBytes(StandardCharsets.UTF_8)));
-                                buildable.value(DateColumn.of("col8", LocalDate.of(2000, 1, 1)));
-                                buildable.value(TimeColumn.of("col9", LocalTime.of(1, 1, 1)));
-                                buildable.value(TimestampColumn.of("col10", LocalDateTime.of(2000, 1, 1, 1, 1)));
-                                buildable.value(TimestampTZColumn.of("col11", Instant.ofEpochMilli(1740041740)));
-                                puts.add(buildable.build());
-                            }
-
-                            for (Put put : puts) {
-                                tx.put(put);
-                            }
-                            tx.commit();
-
-                            int currentInsertedRecords = insertedRecords.addAndGet(batchSize);
-                            if (currentInsertedRecords % 10000 == 0) {
-                                System.out.println("Total records inserted: " + currentInsertedRecords);
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                            tx.abort();  // Ensure rollback on failure
+                        } catch (TransactionException ex) {
+                            ex.printStackTrace();
                         }
-                    });
+                    }
+                    e.printStackTrace();
+                }
+                return null;
+            });
         }
+
         executorService.shutdown();
-        executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        for (int i = 0; i < totalTasks; i++) {
+            try {
+                completionService.take(); // Process each completed task
+            } catch (InterruptedException ignored) {
+            }
+        }
 
     }
 }
---
+++ data-loader/test/datapopulator/src/main/java/com/scalar/db/datapopulator/utils/RandomStringGenerator.java
@@ -0,0 +1,17 @@
+package com.scalar.db.datapopulator.utils;
+
+import java.security.SecureRandom;
+
+public class RandomStringGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public static String generateRandomString(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+
+}
